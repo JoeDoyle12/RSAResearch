04df3df29f9f7a12571643f36f467d6edc806ad6
Aaron Huang <ahuang25@andover.edu>
2024-10-15 10:07:10 -0400

    Update encrypt.py

--- rsa_py/encrypt.py
+++ rsa_py/encrypt.py
@@ -1,6 +1,7 @@
 def encrypt(message, pub):
     """
     Takes a message and uses the Public class to encrypt the message character by character.
+    Returns a list of the encrypted characters.
     """
     encrypted = []
     
