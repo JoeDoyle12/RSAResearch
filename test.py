1d7696eff2078ffa720428846985fce6f7df11c7
Aaron Huang <ahuang25@andover.edu>
2024-10-21 18:29:18 -0400

    Added Labels to Graphs + Saved the Graphs as a Picture

--- test.py
+++ test.py
@@ -16,66 +16,85 @@ hrsa_gen, hrsa_encrypt, hrsa_decrypt = [], [], []
 des_gen, des_encrypt, des_decrypt = [], [], []
 
 for i in range(tests):
-    t = time.time()
-    rsa_test = RSA()
-    rsa_gen.append(time.time() - t)
+    try:
+        t = time.time()
+        rsa_test = RSA()
+        rsa_gen.append(time.time() - t)
 
-    t = time.time()
-    rsa_encrypted = rsa_test.encrypt(str)
-    rsa_encrypt.append(time.time() - t)
+        t = time.time()
+        rsa_encrypted = rsa_test.encrypt(str)
+        rsa_encrypt.append(time.time() - t)
 
-    t = time.time()
-    rsa_decrypted = rsa_test.decrypt(rsa_encrypted)
-    rsa_decrypt.append(time.time() - t)
+        t = time.time()
+        rsa_decrypted = rsa_test.decrypt(rsa_encrypted)
+        rsa_decrypt.append(time.time() - t)
+    except Exception as e:
+        print(f"RSA error on iteration {i}: {e}")
 
-    t = time.time()
-    fernet_test = Fernet()
-    fernet_gen.append(time.time() - t)
+    try:
+        t = time.time()
+        fernet_test = Fernet()
+        fernet_gen.append(time.time() - t)
 
-    t = time.time()
-    fernet_encrypted = fernet_test.encrypt(str)
-    fernet_encrypt.append(time.time() - t)
+        t = time.time()
+        fernet_encrypted = fernet_test.encrypt(str)
+        fernet_encrypt.append(time.time() - t)
 
-    t = time.time()
-    fernet_decrypted = fernet_test.decrypt(fernet_encrypted)
-    fernet_decrypt.append(time.time() - t)
+        t = time.time()
+        fernet_decrypted = fernet_test.decrypt(fernet_encrypted)
+        fernet_decrypt.append(time.time() - t)
+    except Exception as e:
+        print(f"Fernet error on iteration {i}: {e}")
 
-    t = time.time()
-    hrsa = HomeRsa()
-    hrsa_gen.append(time.time() - t)
+    try:
+        t = time.time()
+        hrsa = HomeRsa()
+        hrsa_gen.append(time.time() - t)
 
-    t = time.time()
-    hrsa_encrypted = hrsa.encrypt(str)
-    hrsa_encrypt.append(time.time() - t)
+        t = time.time()
+        hrsa_encrypted = hrsa.encrypt(str)
+        hrsa_encrypt.append(time.time() - t)
 
-    t = time.time()
-    hrsa_decrypted = hrsa.decrypt(hrsa_encrypted)
-    hrsa_decrypt.append(time.time() - t)
+        t = time.time()
+        hrsa_decrypted = hrsa.decrypt(hrsa_encrypted)
+        hrsa_decrypt.append(time.time() - t)
+    except Exception as e:
+        print(f"HomeRSA error on iteration {i}: {e}")
 
-    t = time.time()
-    des = DES()
-    des_gen.append(time.time() - t)
+    try:
+        t = time.time()
+        des = DES()
+        des_gen.append(time.time() - t)
 
-    t = time.time()
-    des_encrypted = des.encrypt(str)
-    des_encrypt.append(time.time() - t)
+        t = time.time()
+        des_encrypted = des.encrypt(str)
+        des_encrypt.append(time.time() - t)
 
-    t = time.time()
-    des_decrypted = des.decrypt(des_encrypted)
-    des_decrypt.append(time.time() - t)
+        t = time.time()
+        des_decrypted = des.decrypt(des_encrypted)
+        des_decrypt.append(time.time() - t)
+    except Exception as e:
+        print(f"DES error on iteration {i}: {e}")
 
 
+def avg(lst):
+    return sum(lst) / len(lst) if lst else 0
 
-def avg(list):
-    return sum(list) / len(list)
+encryption_types = ['RSA', 'Fernet', 'HomeRSA', 'DES']
 
-averages = [avg(rsa_gen), avg(rsa_encrypt), avg(rsa_decrypt), avg(fernet_gen), avg(fernet_encrypt), avg(fernet_decrypt)]
+fig, axs = plt.subplots(1, 3, figsize=(12, 6))
 
-fig, axs = plt.subplots(1, 3, figsize=(8, 6))
+axs[0].bar(encryption_types, [avg(rsa_gen), avg(fernet_gen), avg(hrsa_gen), avg(des_gen)])
+axs[0].set_title('Key Generation Time')
 
-axs[0].bar([0, 1, 2, 3], [avg(rsa_gen), avg(fernet_gen), avg(hrsa_gen), avg(des_gen)])
-axs[1].bar([0, 1, 2, 3], [avg(rsa_encrypt), avg(fernet_encrypt), avg(hrsa_encrypt), avg(des_encrypt)])
-axs[2].bar([0, 1, 2, 3], [avg(rsa_decrypt), avg(fernet_decrypt), avg(hrsa_decrypt), avg(des_decrypt)])
+axs[1].bar(encryption_types, [avg(rsa_encrypt), avg(fernet_encrypt), avg(hrsa_encrypt), avg(des_encrypt)])
+axs[1].set_title('Encryption Time')
+
+axs[2].bar(encryption_types, [avg(rsa_decrypt), avg(fernet_decrypt), avg(hrsa_decrypt), avg(des_decrypt)])
+axs[2].set_title('Decryption Time')
+
+for ax in axs:
+    ax.set_ylabel("Time (seconds)")
 
 plt.tight_layout()
 plt.show()
